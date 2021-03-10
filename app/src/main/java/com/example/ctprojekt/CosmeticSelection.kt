6a58a8e3f4d6af6e698f50738376103ac0d12cc6
CodedInMyHead <38440532+CodedInMyHead@users.noreply.github.com>
2021-03-10 11:21:21 +0100

    Added actual Views

---
+++ app/src/main/java/com/example/ctprojekt/CosmeticSelection.kt
@@ -0,0 +1,11 @@
+package com.example.ctprojekt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CosmeticSelection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cosmetic_selection)
+    }
+}
---
+++ app/src/main/java/com/example/ctprojekt/CosmeticShop.kt
@@ -0,0 +1,11 @@
+package com.example.ctprojekt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CosmeticShop : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cosmetic_shop)
+    }
+}
