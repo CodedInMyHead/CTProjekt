68493f60366bb102e075aacc89256a5adb169396
CodedInMyHead <38440532+CodedInMyHead@users.noreply.github.com>
2021-03-10 09:58:29 +0100

    update

--- app/src/main/java/com/example/ctprojekt/MyTicTacToeCanvas.kt
+++ app/src/main/java/com/example/ctprojekt/MyTicTacToeCanvas.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Button
 import androidx.core.content.res.ResourcesCompat
 
 class MyTicTacToeCanvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
