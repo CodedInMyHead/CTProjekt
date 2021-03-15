0f5ce12a619292111c1b75c7ce8d650c5e868b9b
Kissnele <80387953+Kissnele@users.noreply.github.com>
2021-03-15 20:41:13 +0100

    Snake noch nicht Funktionstüchtig

--- app/src/main/java/com/example/ctprojekt/Snake.kt
+++ app/src/main/java/com/example/ctprojekt/Snake.kt
@@ -1,11 +1,25 @@
 package com.example.ctprojekt
 
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Snake : AppCompatActivity() {
+    var snakeEngine: SnakeEngine? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snake)
+        // Get the pixel dimensions of the screen
+        val display = windowManager.defaultDisplay
+
+        // Initialize the result into a Point object
+        val size = Point()
+        display.getSize(size)
+
+        // Create a new instance of the SnakeEngine class
+        snakeEngine = SnakeEngine(this, size)
+
+        // Make snakeEngine the view of the Activity
+        setContentView(snakeEngine)
     }
 }
